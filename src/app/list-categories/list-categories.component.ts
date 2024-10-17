45f16311a605b7e166fe709d64b6c03fedc15e5b
Oumezzine Farah <oumezzine.farah@yahoo.com>
2024-10-17 11:44:39 +0100

    add model categorie

--- src/app/list-categories/list-categories.component.ts
+++ src/app/list-categories/list-categories.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { Categorie } from '../models/categorie';
+import { CommonModule } from '@angular/common'; /**ngFor directive comes from CommonModule*/
 @Component({
   selector: 'app-list-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule  ],
   templateUrl: './list-categories.component.html',
   styleUrl: './list-categories.component.css'
 })
 export class ListCategoriesComponent {
+  categories : Categorie[]=[
+    {"id":1,"title":"Grand électroménager",
+    "image":"assets/images/categorie_electromenager.jpg", "description":"Qualité top",
+    "available":true},
 
+    {"id":2,"title":"Petit électroménager",
+    "image":"assets/images/categorie_petit_electromenager.jpg", "description":"Tester",
+    "available":true},
+
+    {"id":3,"title":"Produits informatiques",
+    "image":"assets/images/categorie_produits_informatiques.png", "description":"tester 1234",
+    "available":true},
+
+    {"id":4,"title":"Smart Phones", "image":"assets/images/categorie_smartPhone.jpg",
+    "description":"Le nouveau smartphone X est un appareil élégant et performant, doté d'un écran OLED de 6,5 pouces et d'un processeur ultra-rapide", "available":true},
+
+    {"id":5,"title":"TV, images et son",
+    "image":"assets/images/categorie_tv_image_son.jpg", "description":"Parfait ",
+    "available":true},
+
+    {"id":6,"title":"Produits voiture", "image":"assets/images/produits_nettoyages.jpg",
+    "description":"Des produits sans sulfate","available":false},
+
+    ]
 }
---
+++ src/app/models/categorie.ts
@@ -0,0 +1,16 @@
+export class Categorie {
+    id: number;
+    title: string;
+    image: string;
+    description: string;
+    available: boolean;
+  
+
+  constructor(id: number, title: string, image: string, description: string, available: boolean) {
+    this.id = id;
+    this.title = title;
+    this.image = image;
+    this.description = description;
+    this.available = available;
+  }
+}
