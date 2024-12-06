e3bb2adb5d2c1eb352e6fa7e77d1e6bb5dc32b0a
Oumezzine Farah <84034920+farahoumezzine@users.noreply.github.com>
2024-12-06 21:04:49 +0100

    end Routage statique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: NotFoundPageComponent 
+
     }
 ];
