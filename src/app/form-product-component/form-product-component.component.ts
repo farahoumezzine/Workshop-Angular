1dc53941cb201e351218b906f7bd340b6b44e03b
Oumezzine Farah <84034920+farahoumezzine@users.noreply.github.com>
2024-12-28 22:16:10 +0100

    Reactive form done

--- src/app/form-product-component/form-product-component.component.ts
+++ src/app/form-product-component/form-product-component.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
-import { Product } from '../models/product';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-form-product-component',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule], // Changed FormsModule to ReactiveFormsModule
   templateUrl: './form-product-component.component.html',
   styleUrl: './form-product-component.component.css'
 })
+export class FormProductComponentComponent {
+  productForm: FormGroup;
+  listProducts: Product[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(5), Validators.pattern('[a-zA-Z]*')]],
+      image: [''],
+      categoryId: [null, Validators.required],
+      description: [''],
+      price: [null, [Validators.required, Validators.pattern('^\\d+(\\.\\d+)?$')]],
+      brand: [''],
+      promotion: [null, Validators.pattern('^(0|[1-9][0-9]?)$')]
+    });
+  }
+
+  onSubmit() {
+    if (this.productForm.valid) {
+      const newProduct: Product = {
+        id: this.listProducts.length + 1,
+        ...this.productForm.value
+      };
+      this.listProducts.push(newProduct);
+    }
+  }
+}
+
+
+/* Template-driven
 export class FormProductComponentComponent {
   product: Product = {
     id: 0,
@@ -40,3 +69,4 @@ export class FormProductComponentComponent {
     }
   }
 }
+*/
